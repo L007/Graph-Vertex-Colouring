704f0025b45414db4b7610b852ae0e138f4133ba
Brian R <rizqibrian27@gmail.com>
2018-02-06 21:17:06 +0700

    Initial Commit

---
+++ app/src/main/java/nurul/id/graphvertexcolouring/C3Fragment.java
@@ -0,0 +1,30 @@
+package nurul.id.graphvertexcolouring;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class C3Fragment extends Fragment {
+
+
+    public C3Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_c3, container, false);
+        return view;
+    }
+
+}
---
+++ app/src/main/java/nurul/id/graphvertexcolouring/C4Fragment.java
@@ -0,0 +1,30 @@
+package nurul.id.graphvertexcolouring;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class C4Fragment extends Fragment {
+
+
+    public C4Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_c4, container, false);
+        return view;
+    }
+
+}
