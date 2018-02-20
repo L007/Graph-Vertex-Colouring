388eb1d1cdb814e1b273ac0f4ff3a158631fd32c
Brian Rizqi <30972350+brianrizqi@users.noreply.github.com>
2018-02-20 19:21:16 +0700

    Update About.java

--- app/src/main/java/nurul/id/graphvertexcolouring/About.java
+++ app/src/main/java/nurul/id/graphvertexcolouring/About.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class About extends AppCompatActivity {
-
+//test github
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
